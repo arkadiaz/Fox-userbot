373930616ef694bc64d08d812a4d335479d280fd
Arka <96603361+arkadiaz@users.noreply.github.com>
2022-02-04 02:39:10 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -35,8 +35,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/Arkadiaz/Fox-userbot.git
-cd yansen-userbot
+git clone https://github.com/arkadiaz/Fox-userbot.git
+cd Fox-userbot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
