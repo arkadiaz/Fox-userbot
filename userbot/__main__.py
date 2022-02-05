ebfef6e93c5cba4f904a4ec25fdf258ad485e4a1
Arka <96603361+arkadiaz@users.noreply.github.com>
2022-02-06 05:12:33 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -4,47 +4,26 @@
 # you may not use this file except in compliance with the License.
 #
 """ Userbot start point """
-
-from sys import argv
+from sys
 from importlib import import_module
 
-
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import BOT_VER, LOGS, bot
 from userbot.modules import ALL_MODULES
-from telethon.tl.functions.channels import JoinChannelRequest
-
-INVALID_PH = (
-    "\nERROR: The Phone No. entered is INVALID"
-    "\n Tip: Use Country Code along with number."
-    "\n or check your phone number and try again !"
-)
+from userbot.utils.tools import ya_kali_ngga
 
 
-try:
-    bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
+   
+ try:
+     for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
 
+LOGS.info(f"🔰Fox-Userbot🔰 ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+except BaseException as e:
+    LOGS.info(str(e), exc_info=True)
+    sys.exit(1)
 
-async def fox_userbot_on():
-    try:
-        await bot(JoinChannelRequest("@laz1yy"))
-    except BaseException:
-        pass
-
-    try:
-        await bot(JoinChannelRequest("@arkabotupdate"))
-    except BaseException:
-        pass
-
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
-
-LOGS.info(f"🔰fox - Userbot🔰 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
-
-if len(argv) not in (1, 3, 4):
+bot.loop.run_until_complete(ya_kali_ngga())
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
