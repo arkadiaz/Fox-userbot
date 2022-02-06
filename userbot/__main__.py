9d52efad2085244d486e6bfd96adb9b558e35eac
Arka <96603361+arkadiaz@users.noreply.github.com>
2022-02-07 00:00:14 +0700

    tutut

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -16,7 +16,7 @@ try:
     for module_name in ALL_MODULES:
         imported_module = import_module("userbot.modules." + module_name)
         bot.start()
-        LOGS.info(f"🔰Fox-Userbot🔰 ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+        LOGS.info(f"🦊Fox-Userbot🦊 ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
 except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
@@ -27,7 +27,7 @@ async def userbot_on():
         if BOTLOG_CHATID != 0:
             await bot.send_message(
                 BOTLOG_CHATID,
-                f"🔰Fox-Userbot Berhasil Diaktfikan🔰\n━━━━━━━━━━━━━━━\n❃ Bot Of : {ALIVE_NAME}\n❃ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n━━━━━━━━━━━━━━━",
+                f"🦊Fox-Userbot Berhasil Diaktfikan🦊\n━━━━━━━━━━━━━━━\n❃ Bot Of : {ALIVE_NAME}\n❃ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n━━━━━━━━━━━━━━━",
             )
     except Exception as e:
         LOGS.info(str(e))
