5db5f286ff3ea839965ed5ca00a5e83d6a0b85de
arkadiaz <kneko1411@gmail.com>
2022-02-06 01:57:25 +0000

    Fox-Userbot: auto fixes
    Signed-off-by: arkadiaz <kneko1411@gmail.com@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -106,51 +106,51 @@ PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
 PM_LIMIT = int(os.environ.get("PM_LIMIT", 6))
 
 # Send .chatid in any group with all your administration bots (added)
-G_BAN_LOGGER_GROUP=os.environ.get("G_BAN_LOGGER_GROUP", "")
+G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", "")
 if G_BAN_LOGGER_GROUP:
-    G_BAN_LOGGER_GROUP=int(G_BAN_LOGGER_GROUP)
+    G_BAN_LOGGER_GROUP = int(G_BAN_LOGGER_GROUP)
 
 # Heroku Credentials for updater.
-HEROKU_MEMEZ=sb(os.environ.get("HEROKU_MEMEZ", "True"))
-HEROKU_APP_NAME=os.environ.get("HEROKU_APP_NAME", "")
-HEROKU_API_KEY=os.environ.get("HEROKU_API_KEY", "")
+HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "True"))
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
 
 # JustWatch Country
-WATCH_COUNTRY=os.environ.get("WATCH_COUNTRY", "ID")
+WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", "ID")
 
 # Github Credentials for updater and Gitupload.
-GIT_REPO_NAME=os.environ.get("GIT_REPO_NAME", None)
-GITHUB_ACCESS_TOKEN=os.environ.get("GITHUB_ACCESS_TOKEN", None)
+GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
-UPSTREAM_REPO_URL=os.environ.get(
+UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL", "https://github.com/arkadiaz/fox-userbot"
 )
-UPSTREAM_REPO_BRANCH=os.environ.get("UPSTREAM_REPO_BRANCH", "main")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "main")
 
 # Console verbose logging
-CONSOLE_LOGGER_VERBOSE=sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
 # SQL Database URI
-DB_URI=os.environ.get("DATABASE_URL", None)
+DB_URI = os.environ.get("DATABASE_URL", None)
 
 # OCR API key
-OCR_SPACE_API_KEY=os.environ.get(
+OCR_SPACE_API_KEY = os.environ.get(
     "OCR_SPACE_API_KEY") or "12dc42a0ff88957
 
 # remove.bg API key
-REM_BG_API_KEY=os.environ.get(
+REM_BG_API_KEY = os.environ.get(
     "REM_BG_API_KEY") or "ihAEGNtfnVtCsWnzqiXM1GcS"
 
 # Redis URI & Redis Password
-REDIS_URI=os.environ.get("REDIS_URI", None)
-REDIS_PASSWORD=os.environ.get("REDIS_PASSWORD", None)
+REDIS_URI = os.environ.get("REDIS_URI", None)
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
 
 if REDIS_URI and REDIS_PASSWORD:
     try:
-        REDIS_HOST=REDIS_URI.split(":")[0]
-        REDIS_PORT=REDIS_URI.split(":")[1]
-        redis_connection=redis.Redis(
+        REDIS_HOST = REDIS_URI.split(":")[0]
+        REDIS_PORT = REDIS_URI.split(":")[1]
+        redis_connection = redis.Redis(
             host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD
         )
         redis_connection.ping()
@@ -163,147 +163,147 @@ if REDIS_URI and REDIS_PASSWORD:
         )
 
 # Chrome Driver and Headless Google Chrome Binaries
-CHROME_BIN=os.environ.get("CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
-CHROME_DRIVER=os.environ.get("CHROME_DRIVER") or "/usr/bin/chromedriver"
-GOOGLE_CHROME_BIN=os.environ.get(
+CHROME_BIN = os.environ.get("CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
+CHROME_DRIVER = os.environ.get("CHROME_DRIVER") or "/usr/bin/chromedriver"
+GOOGLE_CHROME_BIN = os.environ.get(
     "GOOGLE_CHROME_BIN") or "/usr/bin/google-chrome"
 
 # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID
-NC_LOG_P_M_S=bool(os.environ.get("NC_LOG_P_M_S", False))
+NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", False))
 # send .get_id in any channel to forward all your NEW PMs to this group
-PM_LOGGR_BOT_API_ID=int(os.environ.get("PM_LOGGR_BOT_API_ID", "-100"))
+PM_LOGGR_BOT_API_ID = int(os.environ.get("PM_LOGGR_BOT_API_ID", "-100"))
 
 # OpenWeatherMap API Key
-OPEN_WEATHER_MAP_APPID=os.environ.get(
+OPEN_WEATHER_MAP_APPID = os.environ.get(
     "OPEN_WEATHER_MAP_APPID") or "5ed2fcba931692ec6bd0a8a3f8d84936"
-WEATHER_DEFCITY=os.environ.get("WEATHER_DEFCITY", None)
+WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
 
 # Lydia API
-LYDIA_API_KEY=os.environ.get(
+LYDIA_API_KEY = os.environ.get(
     "LYDIA_API_KEY") or "632740cd2395c73b58275b54ff57a02b607a9f8a4bbc0e37a24e7349a098f95eaa6569e22e2d90093e9c1a9cc253380a218bfc2b7af2e407494502f6fb76f97e"
 
 # For MONGO based DataBase
-MONGO_URI=os.environ.get("MONGO_URI", None)
+MONGO_URI = os.environ.get("MONGO_URI", None)
 
 # set blacklist_chats where you do not want userbot's features
-UB_BLACK_LIST_CHAT=os.environ.get("UB_BLACK_LIST_CHAT", None)
+UB_BLACK_LIST_CHAT = os.environ.get("UB_BLACK_LIST_CHAT", None)
 
 # Anti Spambot Config
-ANTI_SPAMBOT=sb(os.environ.get("ANTI_SPAMBOT", "False"))
-ANTI_SPAMBOT_SHOUT=sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
+ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
+ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 
 # Youtube API key
-YOUTUBE_API_KEY=os.environ.get(
+YOUTUBE_API_KEY = os.environ.get(
     "YOUTUBE_API_KEY") or "AIzaSyACwFrVv-mlhICIOCvDQgaabo6RIoaK8Dg"
 
 # Untuk Perintah .ynsnalive
-ROSE_TEKS_KUSTOM=os.environ.get("FOX_TEKS_KUSTOM", "")
+ROSE_TEKS_KUSTOM = os.environ.get("FOX_TEKS_KUSTOM", "")
 
 # Untuk Mengubah Pesan Welcome
-START_WELCOME=os.environ.get("START_WELCOME", None)
+START_WELCOME = os.environ.get("START_WELCOME", None)
 
 # Default .alive Name
-ALIVE_NAME=os.environ.get("ALIVE_NAME", None)
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
 # Time & Date - Country and Time Zone
-COUNTRY=str(os.environ.get("COUNTRY", "ID"))
-TZ_NUMBER=int(os.environ.get("TZ_NUMBER", 1))
+COUNTRY = str(os.environ.get("COUNTRY", "ID"))
+TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
 
 # Clean Welcome
-CLEAN_WELCOME=sb(os.environ.get("CLEAN_WELCOME", "True"))
+CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
 
 # Zipfile Module
-ZIP_DOWNLOAD_DIRECTORY=os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
+ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
 
 # bit.ly Module
-BITLY_TOKEN=os.environ.get(
+BITLY_TOKEN = os.environ.get(
     "BITLY_TOKEN") or "o_1fpd9299vp"
 
 # Bot Name
-TERM_ALIAS=os.environ.get("TERM_ALIAS", "Geez-UserBot")
+TERM_ALIAS = os.environ.get("TERM_ALIAS", "Geez-UserBot")
 
 # Bot Version
-BOT_VER=os.environ.get("BOT_VER", "7.0")
+BOT_VER = os.environ.get("BOT_VER", "7.0")
 
 # Default .alive Username
-ALIVE_USERNAME=os.environ.get("ALIVE_USERNAME", None)
+ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 
 # Sticker Custom Pack Name
-S_PACK_NAME=os.environ.get("S_PACK_NAME", None)
+S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
 
 # Default .alive Logo
-ALIVE_LOGO=(os.environ.get("ALIVE_LOGO")
+ALIVE_LOGO = (os.environ.get("ALIVE_LOGO")
               or "https://telegra.ph/file/8d1fbe4bef4642956d726.jpg")
 
 # Default .helpme Logo
-INLINE_PIC=(os.environ.get("INLINE_PIC")
+INLINE_PIC = (os.environ.get("INLINE_PIC")
               or "https://telegra.ph/file/8d1fbe4bef4642956d726.jpg")
 
 # Default emoji help
-EMOJI_HELP=os.environ.get("EMOJI_HELP") or "🚀"
+EMOJI_HELP = os.environ.get("EMOJI_HELP") or "🚀"
 
 # Last.fm Module
-BIO_PREFIX=os.environ.get("BIO_PREFIX", None)
-DEFAULT_BIO=os.environ.get("DEFAULT_BIO", None)
+BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
-LASTFM_API=os.environ.get(
+LASTFM_API = os.environ.get(
     "LASTFM_API") or "73d42d9c93626709dc2679d491d472bf"
 
-LASTFM_SECRET=os.environ.get("LASTFM_SECRET", None)
-LASTFM_USERNAME=os.environ.get("LASTFM_USERNAME", None)
-LASTFM_PASSWORD_PLAIN=os.environ.get("LASTFM_PASSWORD", None)
-LASTFM_PASS=md5(LASTFM_PASSWORD_PLAIN)
+LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+LASTFM_PASS = md5(LASTFM_PASSWORD_PLAIN)
 if LASTFM_API and LASTFM_SECRET and LASTFM_USERNAME and LASTFM_PASS:
-    lastfm=LastFMNetwork(
+    lastfm = LastFMNetwork(
         api_key=LASTFM_API,
         api_secret=LASTFM_SECRET,
         username=LASTFM_USERNAME,
         password_hash=LASTFM_PASS,
     )
 else:
-    lastfm=None
+    lastfm = None
 
 # Google Drive Module
-G_DRIVE_DATA=os.environ.get("G_DRIVE_DATA", None)
-G_DRIVE_CLIENT_ID=os.environ.get("G_DRIVE_CLIENT_ID", None)
-G_DRIVE_CLIENT_SECRET=os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-G_DRIVE_AUTH_TOKEN_DATA=os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
-G_DRIVE_FOLDER_ID=os.environ.get("G_DRIVE_FOLDER_ID", None)
-TEMP_DOWNLOAD_DIRECTORY=os.environ.get(
+G_DRIVE_DATA = os.environ.get("G_DRIVE_DATA", None)
+G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
+TEMP_DOWNLOAD_DIRECTORY = os.environ.get(
     "TMP_DOWNLOAD_DIRECTORY", "./downloads")
 # Google Photos
-G_PHOTOS_CLIENT_ID=os.environ.get("G_PHOTOS_CLIENT_ID", None)
-G_PHOTOS_CLIENT_SECRET=os.environ.get("G_PHOTOS_CLIENT_SECRET", None)
-G_PHOTOS_AUTH_TOKEN_ID=os.environ.get("G_PHOTOS_AUTH_TOKEN_ID", None)
+G_PHOTOS_CLIENT_ID = os.environ.get("G_PHOTOS_CLIENT_ID", None)
+G_PHOTOS_CLIENT_SECRET = os.environ.get("G_PHOTOS_CLIENT_SECRET", None)
+G_PHOTOS_AUTH_TOKEN_ID = os.environ.get("G_PHOTOS_AUTH_TOKEN_ID", None)
 if G_PHOTOS_AUTH_TOKEN_ID:
-    G_PHOTOS_AUTH_TOKEN_ID=int(G_PHOTOS_AUTH_TOKEN_ID)
+    G_PHOTOS_AUTH_TOKEN_ID = int(G_PHOTOS_AUTH_TOKEN_ID)
 
 # Genius Lyrics  API
-GENIUS=os.environ.get(
+GENIUS = os.environ.get(
     "GENIUS") or "vDhUmdo_ufwIvEymMeMY65IedjWaVm1KPupdx0L"
 
 
 # Quotes API Token
-QUOTES_API_TOKEN=os.environ.get(
+QUOTES_API_TOKEN = os.environ.get(
     "QUOTES_API_TOKEN") or "33273f18-4a0d-4a76-8d78-a16faa002375"
 
 # Wolfram Alpha API
-WOLFRAM_ID=os.environ.get("WOLFRAM_ID") or None
+WOLFRAM_ID = os.environ.get("WOLFRAM_ID") or None
 
 # Deezloader
-DEEZER_ARL_TOKEN=os.environ.get("DEEZER_ARL_TOKEN", None)
+DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN", None)
 
 # Photo Chat - Get this value from http://antiddos.systems
-API_TOKEN=os.environ.get("API_TOKEN", None)
-API_URL=os.environ.get("API_URL", "http://antiddos.systems")
+API_TOKEN = os.environ.get("API_TOKEN", None)
+API_URL = os.environ.get("API_URL", "http://antiddos.systems")
 
 # Inline bot helper
-BOT_TOKEN=os.environ.get("BOT_TOKEN") or None
-BOT_USERNAME=os.environ.get("BOT_USERNAME") or None
+BOT_TOKEN = os.environ.get("BOT_TOKEN") or None
+BOT_USERNAME = os.environ.get("BOT_USERNAME") or None
 
 # Init Mongo
-MONGOCLIENT=MongoClient(MONGO_URI, 27017, serverSelectionTimeoutMS=1)
-MONGO=MONGOCLIENT.userbot
+MONGOCLIENT = MongoClient(MONGO_URI, 27017, serverSelectionTimeoutMS=1)
+MONGO = MONGOCLIENT.userbot
 
 
 def is_mongo_alive():
@@ -317,7 +317,7 @@ def is_mongo_alive():
 # Init Redis
 # Redis will be hosted inside the docker container that hosts the bot
 # We need redis for just caching, so we just leave it to non-persistent
-REDIS=StrictRedis(host='localhost', port=6379, db=0)
+REDIS = StrictRedis(host='localhost', port=6379, db=0)
 
 
 def is_redis_alive():
@@ -333,7 +333,7 @@ def is_redis_alive():
 if not os.path.exists('bin'):
     os.mkdir('bin')
 
-binaries={
+binaries = {
     "https://raw.githubusercontent.com/adekmaulana/megadown/master/megadown":
       "bin/megadown",
     "https://raw.githubusercontent.com/yshalsager/cmrudl.py/master/cmrudl.py":
@@ -341,17 +341,17 @@ binaries={
 }
 
 for binary, path in binaries.items():
-    downloader=SmartDL(binary, path, progress_bar=False)
+    downloader = SmartDL(binary, path, progress_bar=False)
     downloader.start()
     os.chmod(path, 0o755)
 
 # 'bot' variable
 if STRING_SESSION:
-    session=StringSession(str(STRING_SESSION))
+    session = StringSession(str(STRING_SESSION))
 else:
-    session="yansen-userbot"
+    session = "yansen-userbot"
 try:
-    bot=TelegramClient(
+    bot = TelegramClient(
         session=session,
         api_id=API_KEY,
         api_hash=API_HASH,
